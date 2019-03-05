56dc861d43152c35164c6656c35ac7d9389d91df
Simon grahn <simon.grahn@gmail.com>
2019-03-05 13:59:32 +0100

    first push

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'mr. Nice' },
+  { id: 12, name: 'mr.Cool' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'mr. Awesome' },
+  { id: 15, name: 'x-ray' },
+  { id: 16, name: 'mr. Dude' },
+  { id: 17, name: 'mr. Awesome' },
+  { id: 18, name: 'dr. IQ' },
+  { id: 19, name: 'Tornado' },
+  { id: 20, name: 'Magma' }
+];
